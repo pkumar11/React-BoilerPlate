db41788ae46d78d416cbe24bd2285d0f71d00986
studentpankaj <pankaj10101993@gmail.com>
2019-07-08 10:47:33 +0530

    React boilerplate

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello from index.js</h1>
+            </div>
+        )
+    }
+}
+ReactDOM.render(<App />,document.getElementById("root"));
